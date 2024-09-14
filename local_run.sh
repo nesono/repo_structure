293f50942e24dd7fab097ff2ec3a72e2138e4702
Jochen Issing <c.333+git@nesono.com>
2024-09-14 21:46:45 +0200

    Add local_run.sh for setting up and running the local environment
    This script ensures the correct Python version is used and sets up a virtual environment. It installs required dependencies and runs the main application with specified configurations.

---
+++ local_run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+PY_VERSION=3.12
+PY_BINARY="$(which python${PY_VERSION})" || PY_BINARY=true
+
+if [[ $($PY_BINARY --version) != "Python ${PY_VERSION}"* ]]; then
+  echo "Error: Python ${PY_VERSION} binary not found" >&2
+  exit 1
+fi
+
+trap 'echo "An error occurred. Exiting..." >&2; deactivate; exit 1' ERR
+
+$PY_BINARY -m venv temp_venv
+source temp_venv/bin/activate
+
+{
+  pip install -r requirements_lock.txt > pip_install.log 2>&1
+} || {
+  echo "Failed to install dependencies" >&2
+  exit 1
+}
+rm -f pip_install.log
+
+python main.py --repo-root=. --config-path=repo_structure_config.yaml --verbose
