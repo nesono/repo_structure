ef161d0e79934583e3a0bfc696a29681e113fff0
Jochen Issing <c.333+github@nesono.com>
2024-10-31 12:19:35 +0100

    Refactor recursive repo structure validation
    Restructured the recursive function `_fail_if_invalid_repo_structure_recursive` to enhance readability and maintainability. Simplified and modularized related helper functions, and adjusted indentation for improved code clarity.
    
    Signed-off-by: Jochen Issing <c.333+github@nesono.com>

--- repo_structure/repo_structure_enforcement.py
+++ repo_structure/repo_structure_enforcement.py
@@ -90,6 +90,7 @@ def _fail_if_required_entries_missing(
     if missing_required:
         missing_required_files = [f.path for f in missing_required if not f.is_dir]
         missing_required_dirs = [d.path for d in missing_required if d.is_dir]
+
         raise MissingRequiredEntriesError(
             f"Required entries missing:\nFiles: "
             f"{missing_required_files}\nDirs: {missing_required_dirs}"
@@ -105,47 +106,9 @@ class Entry:
     is_symlink: bool
 
 
-def _to_entry(entry: DirEntry[str], rel_path) -> Entry:
-    return Entry(rel_path, entry.is_dir(), entry.is_symlink())
-
-
-def _fail_if_invalid_repo_structure_recursive(
-    repo_root: str,
-    rel_dir: str,
-    config: Configuration,
-    backlog: StructureRuleList,
-    flags: Flags,
-) -> None:
-    git_ignore = _get_git_ignore(repo_root)
-
-    for entry in os.scandir(os.path.join(repo_root, rel_dir)):
-        rel_path = os.path.join(rel_dir, entry.name)
-        if flags.verbose:
-            print(f"Checking entry {rel_path}")
-
-        if _skip_entry(_to_entry(entry, rel_path), config, git_ignore, flags):
-            continue
-
-        for idx in _get_matching_item_index(
-            backlog,
-            rel_path,
-            entry.is_dir(),
-            flags.verbose,
-        ):
-            backlog_entry = backlog[idx]
-            backlog_entry.count += 1
-            if flags.verbose:
-                print(f"  Registered usage for path {rel_path}")
-
-            if entry.is_dir():
-                _handle_use_rule(
-                    backlog, backlog_entry.use_rule, config, flags, rel_path
-                )
-                _handle_if_exists(backlog, backlog_entry, rel_path, flags)
-
-                _fail_if_invalid_repo_structure_recursive(
-                    repo_root, rel_path, config, backlog, flags
-                )
+def _to_entry(os_entry: DirEntry[str], rel_dir: str) -> Entry:
+    rel_path = os.path.join(rel_dir, os_entry.name)
+    return Entry(rel_path, os_entry.is_dir(), os_entry.is_symlink())
 
 
 def _handle_use_rule(
@@ -179,13 +142,6 @@ def _handle_if_exists(
             )
 
 
-def _get_git_ignore(repo_root: str) -> Union[Callable[[str], bool], None]:
-    git_ignore_path = os.path.join(repo_root, ".gitignore")
-    if os.path.isfile(git_ignore_path):
-        return parse_gitignore(git_ignore_path)
-    return None
-
-
 def _skip_entry(
     entry: Entry,
     config: Configuration,
@@ -211,16 +167,61 @@ def _skip_entry(
     return False
 
 
+def _fail_if_invalid_repo_structure_recursive(
+    repo_root: str,
+    rel_dir: str,
+    config: Configuration,
+    backlog: StructureRuleList,
+    flags: Flags,
+) -> None:
+
+    def _get_git_ignore(rr: str) -> Union[Callable[[str], bool], None]:
+        git_ignore_path = os.path.join(rr, ".gitignore")
+        if os.path.isfile(git_ignore_path):
+            return parse_gitignore(git_ignore_path)
+        return None
+
+    git_ignore = _get_git_ignore(repo_root)
+
+    for os_entry in os.scandir(os.path.join(repo_root, rel_dir)):
+        entry = _to_entry(os_entry, rel_dir)
+
+        if flags.verbose:
+            print(f"Checking entry {entry.path}")
+
+        if _skip_entry(entry, config, git_ignore, flags):
+            continue
+
+        for idx in _get_matching_item_index(
+            backlog,
+            entry.path,
+            os_entry.is_dir(),
+            flags.verbose,
+        ):
+            backlog_entry = backlog[idx]
+            backlog_entry.count += 1
+            if flags.verbose:
+                print(f"  Registered usage for path {entry.path}")
+
+            if os_entry.is_dir():
+                _handle_use_rule(
+                    backlog, backlog_entry.use_rule, config, flags, entry.path
+                )
+                _handle_if_exists(backlog, backlog_entry, entry.path, flags)
+
+                _fail_if_invalid_repo_structure_recursive(
+                    repo_root, entry.path, config, backlog, flags
+                )
+
+
 def _map_dir_to_entry_backlog(
     config: Configuration,
     map_dir: str,
 ) -> StructureRuleList:
 
-    def _get_use_rules_for_directory(
-        config: Configuration, directory: str
-    ) -> List[str]:
+    def _get_use_rules_for_directory(c: Configuration, directory: str) -> List[str]:
         d = rel_dir_to_map_dir(directory)
-        return config.directory_map[d]
+        return c.directory_map[d]
 
     use_rules = _get_use_rules_for_directory(config, map_dir)
     return _build_active_entry_backlog(use_rules, map_dir, config)
@@ -299,19 +300,6 @@ def _assert_path_in_backlog(
                 print(f"Found entry in backlog with index {idx}")
 
 
-def _get_corresponding_map_dir(config: Configuration, flags: Flags, path: str):
-
-    max_map_dir = ""
-    for sub_path, is_dir in _incremental_path_split(path):
-        if is_dir and sub_path in config.directory_map:
-            max_map_dir = sub_path
-
-    if flags.verbose:
-        print(f"Found corresponding map dir for {path}: {max_map_dir}")
-
-    return max_map_dir
-
-
 def assert_path(
     config: Configuration,
     path: str,
@@ -322,9 +310,20 @@ def assert_path(
     Note that this function will not be able to ensure if all required
     entries are present."""
 
-    max_map_dir = _get_corresponding_map_dir(config, flags, path)
+    def _get_corresponding_map_dir(c: Configuration, f: Flags, p: str):
 
-    rel_dir = map_dir_to_rel_dir(max_map_dir)
-    backlog = _map_dir_to_entry_backlog(config, rel_dir)
+        map_dir = ""
+        for sub_path, is_dir in _incremental_path_split(p):
+            if is_dir and sub_path in c.directory_map:
+                map_dir = sub_path
+
+        if f.verbose:
+            print(f"Found corresponding map dir for {p}: {map_dir}")
+
+        return map_dir
+
+    backlog = _map_dir_to_entry_backlog(
+        config, map_dir_to_rel_dir(_get_corresponding_map_dir(config, flags, path))
+    )
 
     _assert_path_in_backlog(backlog, config, flags, path)
