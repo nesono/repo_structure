36a12b37fb5e89292d14e800c4c1c44d88d8643e
nesono <nesono@users.noreply.github.com>
2024-12-29 13:52:27 +0100

    Remove unused README.md from repo_struct folder (#58)
    Signed-off-by: Jochen Issing <c.333+git@nesono.com>
    Co-authored-by: Jochen Issing <c.333+git@nesono.com>

--- repo_structure/__main___test.py
+++ repo_structure/__main___test.py
@@ -62,7 +62,6 @@ def test_main_diff_scan_success():
             "repo_structure/test_config_allow_all.yaml",
             "LICENSE",
             "repo_structure.yaml",
-            "repo_struct/README.md",
             "repo_structure/repo_structure_config.py",
         ],
     )
